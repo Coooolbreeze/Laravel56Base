52ce1dc950acca5687add61fd07e673770065b34
Coooolbreeze <392113643@qq.com>
2018-07-26 16:31:01 +0800

    添加uuid公共方法

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -114,6 +114,23 @@ function makeOrderNo()
 }
 
 /**
+ * 生成UUID
+ *
+ * @param string $prefix
+ * @return string
+ */
+function uuid($prefix = '')
+{
+    $chars = md5(uniqid(mt_rand(), true));
+    $uuid = substr($chars, 0, 8) . '-';
+    $uuid .= substr($chars, 8, 4) . '-';
+    $uuid .= substr($chars, 12, 4) . '-';
+    $uuid .= substr($chars, 16, 4) . '-';
+    $uuid .= substr($chars, 20, 12);
+    return $prefix . $uuid;
+}
+
+/**
  * 获取指定年月第一天的时间戳
  *
  * @param string $year
